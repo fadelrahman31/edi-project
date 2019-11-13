47ca3676c1924111260483c8f907714be874c4ee
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2019-11-13 16:24:23 +0700

    Update for local db access + Nuxt Store

---
+++ pages/confirmation.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+        <v-content>
+            <span>
+                <success-page></success-page>
+            </span>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+import SuccessPage from '@/components/SuccessPage';
+
+export default {
+    name: 'confirmation',
+    components: {
+        AppNavigation,
+        SuccessPage
+    }
+};
+</script>
+
+<style></style>
